bdfd06089c90ef761bacd1ce52c6fca3a68e227d
masniloy <masaimunniloy88@gmail.com>
2022-09-04 20:55:32 +0600

    working on modal

--- news.js
+++ news.js
@@ -36,34 +36,65 @@ const allNewses = newses => {
         const addNews = document.createElement('div');
         addNews.classList.add('news')
         addNews.innerHTML = `
-        <div class=" m-5">
-            <div class="card mb-3">
-            <img src="${news.image_url}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${news.title}</h5>
-                <p class="card-text">${news.details.slice(0, 400)}</p>
-
-                <section class="d-flex justify-content-between">
-
-                <div class="h-25 w-25 d-flex justify-content-start"> 
-                <img src="${news.author.img}" class="card-img-top h-25 w-25" alt="...">
-                <p class="card-text m-2"><small class="text-muted">${news.author.name}<br>${news.author.published_date}</small></p>
-                </div>
-
+        <div  class=" m-5">
+        <div onclick="forModat('${news._id}')" class="card mb-3">
+        <img src="${news.image_url}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${news.title}</h5>
+            <p class="card-text">${news.details.slice(0, 200)}</p>
+            <section class="d-flex justify-content-between">
                 <div class="h-25 w-25 d-flex justify-content-start">
-                <p class="card-text m-2"><small class="text-muted"><i class="fa-solid fa-eye"></i> ${news.total_view}</small></p>
+                    <img src="${news.author.img}" class="card-img-top h-25 w-25" alt="...">
+                    <p class="card-text m-2"><small
+                            class="text-muted">${news.author.name}<br>${news.author.published_date}</small></p>
                 </div>
-
-                <div class="d-flex justify-content-end">
-                <i class="fa-solid fa-caret-down"></i>
+                <div class="h-25 w-25 d-flex justify-content-start">
+                    <p class="card-text m-2"><small class="text-muted"><i class="fa-solid fa-eye"></i>
+                            ${news.total_view}</small></p>
                 </div>
-                </section>
-            </div>
+                
+            </section>
+        </div>
         </div>
         `;
         newsContainer.appendChild(addNews);
     });
 }
+
+const forModat = (mCode) => {
+    console.log(mCode);
+    const url = (`https://openapi.programming-hero.com/api/news/${mCode}`);
+    console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => modal(data.data));
+}
+
+
+
+const modal = mod => {
+    console.log(mod);
+    const checkModal = document.getElementById('modal')
+    const addModal = document.createElement('div')
+    addModal.classList.add('modal-dialog')
+    addModal.innerHTML = `
+    <div class="modal-header">
+            <h5 class="modal-title">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <p>Modal body text goes here.</p>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+    </div>                     
+    `;
+
+}
+
+
+
 
 allCatagories();
 allNews();
